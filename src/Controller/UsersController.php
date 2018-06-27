f834ed9a54233c40f074f1ec4eaf61153b899c53
mrStha <shrs_samar@yahoo.com>
2018-06-27 18:49:37 +0300

    login App commit

---
+++ src/Controller/UsersController.php
@@ -0,0 +1,147 @@
+<?php
+namespace App\Controller;
+use App\Controller\AppController;
+use Cake\Datasource\ConnectionManager;
+use Cake\Event\Event;
+use Cake\Auth\DefaultPasswordHasher;
+
+/**
+ * Users Controller
+ *
+ * @property \App\Model\Table\UsersTable $Users
+ *
+ * @method \App\Model\Entity\User[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class UsersController extends AppController
+{
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|void
+     */
+     public function beforeFilter(Event $event){
+    $this->Auth->allow(['controller'=>'Users','action'=>'index']);
+     return parent::beforeFilter($event);
+  }
+
+
+    public function index()
+    {
+        $users = $this->paginate($this->Users);
+        $this->set(compact('users'));
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id User id.
+     * @return \Cake\Http\Response|void
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $user = $this->Users->get($id, [
+            'contain' => []
+        ]);
+
+        $this->set('user', $user);
+    }
+
+    public function login(){
+      if($this->request->is('post')){
+
+        $user = $this->request->getData();
+        $formname = $user['username'];
+    /*    $PasswordHasher = new DefaultPasswordHasher();
+        $password = $PasswordHasher->hash($user['password']);
+        echo "your password is ".$password;*///the hasher returns different value each time for same password ??
+    $query = $this->Users
+        ->find()
+        ->select(['id', 'username','password'])
+        ->where(['email'=>$user['username'],
+        'AND'   =>['password' => $user['password']],]);
+      //  debug($query);
+        $number = $query->count();
+      if($number==1){
+        $this->Flash->success('Greato!!');
+        $this->Auth->setUser($user);
+        return $this->redirect($this->Auth->redirectUrl());
+        //$this->redirect(['action' =>'index']);
+      }else{
+        $this->Flash->error('Sorry Bro!!');
+        $this->redirect(['action' =>'login']);
+
+      }
+
+      }
+
+    }
+
+
+//
+
+   /**
+     * Add method
+     *
+     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        $user = $this->Users->newEntity();
+        if ($this->request->is('post')) {
+            $user = $this->Users->patchEntity($user, $this->request->getData());
+            if ($this->Users->save($user)) {
+                $this->Flash->success(__('The user has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The user could not be saved. Please, try again.'));
+        }
+        $this->set(compact('user'));
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id User id.
+     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     */
+    public function edit($id = null)
+    {
+        $user = $this->Users->get($id, [
+            'contain' => []
+        ]);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $user = $this->Users->patchEntity($user, $this->request->getData());
+            if ($this->Users->save($user)) {
+                $this->Flash->success(__('The user has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The user could not be saved. Please, try again.'));
+        }
+        $this->set(compact('user'));
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id User id.
+     * @return \Cake\Http\Response|null Redirects to index.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function delete($id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $user = $this->Users->get($id);
+        if ($this->Users->delete($user)) {
+            $this->Flash->success(__('The user has been deleted.'));
+        } else {
+            $this->Flash->error(__('The user could not be deleted. Please, try again.'));
+        }
+
+        return $this->redirect(['action' => 'index']);
+    }
+}
